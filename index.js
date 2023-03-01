334126112bd0a0edda6779cb185c492147f021c6
Kunal <61162569+KunalLorex@users.noreply.github.com>
2023-03-01 12:39:38 +0530

    added cered for n
    checked for vulnerabilities

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.post('/users',(req,res)=>{
         host:'mail.google.com',
         service: 'gmail',
         auth: {
-          user: 'tutoring@mymegaminds.com',
-          pass: 'wsvftbbumwrtvdlh'
+          user: 'kaushakhilish@gmail.com',
+          pass: 'okqwnotejopyidnt'
          
         }
     });
